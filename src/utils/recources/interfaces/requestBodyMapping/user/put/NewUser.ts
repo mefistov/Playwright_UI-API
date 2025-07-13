039de2940bc3e29a79ecbfc65b969b21382e673a
vladyslavseden <vlad.seden@gmail.com>
2025-07-13 17:41:53 +0200

    tests structure test steps and test files added

---
+++ src/utils/recources/interfaces/requestBodyMapping/user/put/NewUser.ts
@@ -0,0 +1,10 @@
+export interface NewUser {
+    "id": number;
+    "username": string;
+    "firstName": string;
+    "lastName": string;
+    "email": string;
+    "password": string;
+    "phone": string;
+    "userStatus": number;
+}
